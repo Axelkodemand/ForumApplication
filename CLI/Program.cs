21636ed645ca519d958101265ee5811291574d68
Axelkodemand <axel.jarland@gmail.com>
2026-09-11 12:49:26 +0200

    added missing csproj files

---
+++ CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+
+Console.WriteLine ("Starting CLI application");
+IUserRepository userRepository = new UserInMemoryRepository();
+ICommentRepository commentRepository = new CommentInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+
+CliUiApp cliUiApp = new CliUiApp(userRepository, commentRepository, postRepository);
+await cliUiApp.StartAsync();
