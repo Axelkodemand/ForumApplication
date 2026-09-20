24f6b06c0bfcf4122207f4414d6340e89fe2925c
Axelkodemand <axel.jarland@gmail.com>
2026-09-20 10:42:39 +0200

    Assignment 3

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,12 +1,11 @@
 ﻿using CLI.UI;
-using InMemoryRepositories;
+using FileRepositories;
 using RepositoryContracts;
 
-
-Console.WriteLine ("Starting CLI application");
-IUserRepository userRepository = new UserInMemoryRepository();
-ICommentRepository commentRepository = new CommentInMemoryRepository();
-IPostRepository postRepository = new PostInMemoryRepository();
+Console.WriteLine("Starting CLI application");
+IUserRepository userRepository = new UserFileRepository();
+ICommentRepository commentRepository = new CommentFileRepository();
+IPostRepository postRepository = new PostFileRepository();
 
 CliUiApp cliUiApp = new CliUiApp(userRepository, commentRepository, postRepository);
 await cliUiApp.StartAsync();
